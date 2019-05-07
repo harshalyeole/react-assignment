dbc7d688c853409c8ef15a693e5566c2dce48f6c
Yeole <Harshal.Yeole@int.asurion.com>
2019-05-07 17:34:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Col, Container, Row } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      backgroundcolor1: 'red',
+      backgroundcolor2: 'green',
+      backgroundcolor3: 'blue',
+      rSelected: 1
+    }
+  }
+  
+  changeColor = (input) => {
+    this.setState({
+      backgroundcolor1: (input === "rgb") ? "red" : ((input === "gbr") ? "green" : "blue"),
+      backgroundcolor2: (input === "rgb") ? "green" : ((input === "gbr") ? "blue" : "red"),
+      backgroundcolor3: (input === "rgb") ? "blue" : ((input === "gbr") ? "red" : "green"),
+      rSelected:  (input === "rgb") ? 1 : ((input === "gbr") ? 2 : 3)
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container className="rgb-assignment-container">
+            <div className="container-title"> Assignment 1: State and Props </div>
+            <Row>
+              <Col md="4">
+                <Button color="danger" block onClick={(e) => this.changeColor("rgb")} active={this.state.rSelected === 1}>RGB</Button>{' '}
+              </Col>
+              <Col md="4">
+                <Button color="success" block onClick={(e) => this.changeColor("gbr")} active={this.state.rSelected === 2}>GBR</Button>{' '}
+              </Col>
+              <Col md="4">
+                <Button color="primary" block onClick={(e) => this.changeColor("brg")} active={this.state.rSelected === 3}>BRG</Button>{' '}
+              </Col>
+            </Row>
+            <Row>
+              <Col md="4" className='color-container' >
+                <div className='show-colors' style={{backgroundColor: this.state.backgroundcolor1}} />
+              </Col>
+              <Col md="4" className='color-container' >
+                <div className='show-colors' style={{backgroundColor: this.state.backgroundcolor2}} />
+              </Col>
+              <Col md="4" className='color-container' >
+                <div className='show-colors' style={{backgroundColor: this.state.backgroundcolor3}} />
+              </Col>
+            </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
